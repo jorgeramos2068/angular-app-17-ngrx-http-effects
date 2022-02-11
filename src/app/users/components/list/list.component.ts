154f4f000521e3cbf3195709d25ce65d989855d1
Jorge Ramos <jramos@intersysconsulting.com>
2022-02-11 15:48:36 -0600

    Create service to get api data

--- src/app/users/components/list/list.component.ts
+++ src/app/users/components/list/list.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/interfaces/User.interface';
+
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
 })
 export class ListComponent implements OnInit {
-  constructor() {}
+  public users: User[] = [];
+  constructor(private userService: UserService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe((users) => {
+      this.users = users;
+    });
+  }
 }
