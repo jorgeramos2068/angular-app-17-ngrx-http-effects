52bc791efe2e40f7dd0dd41bc711ee1b0caa39c0
Jorge Ramos <jorgeramos2068@gmail.com>
2023-07-24 14:44:39 -0600

    Create routes file

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {}
+
+  search(id: string): void {
+    if (!id) {
+      return;
+    }
+    this.router.navigateByUrl(`/user/${id}`);
+  }
 }
